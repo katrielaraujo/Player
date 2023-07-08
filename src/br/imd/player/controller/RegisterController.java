fd7163b1afac064e145fbf4e0046e51c29d8f074
Luis <luiseduardofc1500@gmail.com>
2023-07-07 21:14:07 -0300

    Ajuste de prompts e direcionamento para tela de login

--- src/br/imd/player/controller/RegisterController.java
+++ src/br/imd/player/controller/RegisterController.java
@@ -5,10 +5,14 @@ import br.imd.player.model.User;
 import br.imd.player.model.UserVip;
 import br.imd.player.util.UserType;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.CheckBox;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.TextField;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.URL;
 import java.util.Map;
 
 public class RegisterController {
@@ -26,13 +30,12 @@ public class RegisterController {
 
     @FXML
     private void initialize() {
-        // Aqui você pode adicionar lógica de inicialização, se necessário
+
     }
 
     @FXML
     private void handleRegistrarButton() {
-    	User user;
-    	
+        User user;
         boolean isVip = vipButton.isSelected();
         String email = emailField.getText();
         String senha = senhaField.getText();
@@ -40,16 +43,21 @@ public class RegisterController {
         if (isVip){
             user = new UserVip();
         }else {
-        	user = new UserVip();
+            user = new UserVip();
         }
-        
+
         user.setEmail(email);
         user.setPassword(senha);
         user.setType(isVip ? UserType.VIP: UserType.REGULAR);
 
         if (saveUser(user)) {
+            exibirAviso("Sucesso no cadastro", "Usuário salvo: " + email );
             System.out.println("Usuário salvo: " + email);
+            returnLoginScreen();
+
+
         } else {
+            exibirAviso("Fracasso no cadastro", "Usuário já existe: " + email );
             System.out.println("Usuário já existe: " + email);
         }
     }
@@ -58,11 +66,34 @@ public class RegisterController {
         MediaManager media = new MediaManager();
         Map<String,User> users = media.getAllUsers();
         if(users.containsKey(user.getEmail())) {
-        	return false;
+            return false;
         }
-        
+
         media.insertUser(user);
         return true;
+    }
+
+
+    private void exibirAviso(String titulo, String mensagem) {
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle(titulo);
+        alert.setHeaderText(null);
+        alert.setContentText(mensagem);
+        alert.showAndWait();
+    }
+
+    private void returnLoginScreen() {
+        try {
+            URL fileFXML = getClass().getResource("/br/imd/player/view/Login.fxml");
+            Parent loginRoot = FXMLLoader.load(fileFXML);
+
+            Scene loginScene = new Scene(loginRoot);
+            Stage primaryStage = (Stage) registrarButton.getScene().getWindow();
+            primaryStage.setScene(loginScene);
+            primaryStage.setTitle("Login");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 
